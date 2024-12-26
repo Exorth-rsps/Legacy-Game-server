aefb3d8e9d79a022f463729ded06ad4bce802376
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-12-26 22:36:01 +0100

    Update stairs.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/general/stairs.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/general/stairs.plugin.kts
@@ -3,20 +3,20 @@ package gg.rsmod.plugins.content.areas.yanille
  * @author Eikenb00m <https://github.com/eikenb00m>
  */
 
-//on_obj_option(obj = Objs.STAIRCASE_15645, option = "climb-Up") {
-//    when(player.tile.height) {
-//        1 -> {
-//            player.moveTo(x = 2591, z = 3087, 2)
-//        }
-//        else ->  player.moveTo(x = 2591, z = 3092, 1)
-//    }
-//}
-//on_obj_option(obj = Objs.STAIRCASE_15648, option = "climb-down") {
-//    when(player.tile.height) {
-//        1 -> {
-//            player.moveTo(x = 2591, z = 3088, 0)
-//        }
-//        else ->  player.moveTo(x = 2591, z = 3083, 1)
-//    }
-//}
+on_obj_option(obj = Objs.STAIRCASE_15645, option = "climb-Up") {
+    when(player.tile.height) {
+        1 -> {
+            player.moveTo(x = 2680, z = 1637, 2)
+        }
+        else ->  player.moveTo(x = 2680, z = 1642, 1)
+    }
+}
+on_obj_option(obj = Objs.STAIRCASE_15648, option = "climb-down") {
+    when(player.tile.height) {
+        1 -> {
+            player.moveTo(x = 2681, z = 1638, 0)
+        }
+        else ->  player.moveTo(x = 2680, z = 1633, 1)
+    }
+}
 
