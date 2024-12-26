f3d5f7a7708a81212f3b9d0bf63081347f5095bb
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-12-26 22:06:55 +0100

    Create ladders.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/general/ladders.plugin.kts
@@ -0,0 +1,31 @@
+package gg.rsmod.plugins.content.areas.yanille
+/**
+ * @author Eikenb00m <https://github.com/eikenb00m>
+ */
+//Basement
+//on_obj_option(obj = Objs.LADDER_17384, option = "Climb-Down") {
+//    when(player.tile.regionId) {
+//        11317 -> { // Water obelisk
+//            player.moveTo(x = 2842, z = 9823)
+//        }
+//
+//        else -> player.moveTo(2594, 9486)
+//    }
+//}
+//on_obj_option(obj = Objs.LADDER_17385, option = "Climb-Up")
+//{
+//    when(player.tile.regionId) {
+//        11418 -> { // IceQueen Dungeon
+//            player.moveTo(x = 2846, z = 3516)
+//        }
+//        11673 -> { // Taverley Dungeon
+//            player.moveTo(x = 2884, z = 3398)
+//        }
+//        11417 -> { // Water obelisk
+//            player.moveTo(x = 2842, z = 3423)
+//        }
+//
+//        else ->  player.moveTo(x = 2594, z = 3086) //Magic Guild
+//    }
+//}
+
