dd5f122c0b6097936a9f499fd7666d1749d5a1fc
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-12-26 22:06:53 +0100

    Create stairs.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/general/stairs.plugin.kts
@@ -0,0 +1,22 @@
+package gg.rsmod.plugins.content.areas.yanille
+/**
+ * @author Eikenb00m <https://github.com/eikenb00m>
+ */
+
+//on_obj_option(obj = Objs.STAIRCASE_15645, option = "climb-Up") {
+//    when(player.tile.height) {
+//        1 -> {
+//            player.moveTo(x = 2591, z = 3087, 2)
+//        }
+//        else ->  player.moveTo(x = 2591, z = 3092, 1)
+//    }
+//}
+//on_obj_option(obj = Objs.STAIRCASE_15648, option = "climb-down") {
+//    when(player.tile.height) {
+//        1 -> {
+//            player.moveTo(x = 2591, z = 3088, 0)
+//        }
+//        else ->  player.moveTo(x = 2591, z = 3083, 1)
+//    }
+//}
+
