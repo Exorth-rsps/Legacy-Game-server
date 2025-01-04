ece19a0cf24833ca9726e62b5422d500dafa3471
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-04 11:14:23 +0100

    Remove magic guild door code, not needed anymore

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/magic_guild.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/magic_guild.plugin.kts
@@ -3,40 +3,6 @@ package gg.rsmod.plugins.content.areas.yanille
  * @author Eikenb00m <https://github.com/eikenb00m>
  */
 
-// Doors
-//To-Do:    Close doors after opening, the closing wont work atm.
-//          Also need to move player when opening.
-//          When players now try to close, this plugin trows an error.
-//          Nothing is failing after the error.
-val DOORS = intArrayOf(Objs.MAGIC_GUILD_DOOR, Objs.MAGIC_GUILD_DOOR_1733)
-
-DOORS.forEach { obj ->
-    on_obj_option(obj, "Open", lineOfSightDistance = 2) {
-        if (obj == 1732) {
-            player.lock()
-            world.queue {
-                world.openDoor(world.getObject(Tile(2686, 1638), 0)!!, Objs.MAGIC_GUILD_DOOR, invertRot = true)
-                world.openDoor(world.getObject(Tile(2686, 1638), 0)!!, Objs.MAGIC_GUILD_DOOR_1733)
-                world.openDoor(world.getObject(Tile(2673, 1637), 0)!!, Objs.MAGIC_GUILD_DOOR, invertRot = true)
-                world.openDoor(world.getObject(Tile(2673, 1637), 0)!!, Objs.MAGIC_GUILD_DOOR_1733)
-
-            }
-            player.unlock()
-        }
-        if (obj == 1733) {
-            player.lock()
-            world.queue {
-                world.openDoor(world.getObject(Tile(2686, 1637), 0)!!, Objs.MAGIC_GUILD_DOOR, invertRot = true)
-                world.openDoor(world.getObject(Tile(2686, 1638), 0)!!, Objs.MAGIC_GUILD_DOOR_1733)
-                world.openDoor(world.getObject(Tile(2673, 1638), 0)!!, Objs.MAGIC_GUILD_DOOR, invertRot = true)
-                world.openDoor(world.getObject(Tile(2673, 1637), 0)!!, Objs.MAGIC_GUILD_DOOR_1733)
-
-            }
-            player.unlock()
-        }
-    }
-}
-
 //Portals
 //East Portal
 on_obj_option(obj = Objs.MAGIC_PORTAL, option = "Enter") {
