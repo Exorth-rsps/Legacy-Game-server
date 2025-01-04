0e2be5a8df0e9de3fe5006c350385451805485e8
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-04 12:05:54 +0100

    Fixed yell command

--- game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/all/yell.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/all/yell.plugin.kts
@@ -1,35 +1,46 @@
-/**
- * @author Fritz <frikkipafi@gmail.com>
- */
-
 on_command("yell", description = "Yell to everyone") {
 
     val rank: String
     val color: String
 
     when (player.privilege.id) {
-        0 -> { rank = "Player"
-               color = ""}
-        1 -> { rank = "<img=0>Moderator"
-               color = "<shad=16711680>"}
-        2 -> { rank = "<img=1>Admin"
-               color = "<shad=65280>"}
-        3 -> { rank = "<img=21>Developer"
-               color = "<shad=53247>"}
-        4 -> { rank = "<img=1>Owner"
-               color = "<shad=16777215>"}
-        5 -> { rank = "<img=8>Donator"
-               color = "<shad=16711680>"}
-
-    else -> { rank = "unidentified"
-              color = ""}
+        0 -> {
+            rank = "Player"
+            color = ""
+        }
+        1 -> {
+            rank = "<img=0>Moderator"
+            color = "<shad=8388736>"
+        }
+        2 -> {
+            rank = "<img=1>Admin"
+            color = "<shad=139>"
+        }
+        3 -> {
+            rank = "<img=1>Developer"
+            color = "<shad=12850>"
+        }
+        4 -> {
+            rank = "<img=1>Owner"
+            color = "<shad=139>"
+        }
+        5 -> {
+            rank = "<img=8>Donator"
+            color = "<shad=16776960>"
+        }
+        else -> {
+            rank = "unidentified"
+            color = ""
+        }
     }
 
+    // Combineer alle argumenten in één string
     val args = player.getCommandArgs()
     val name = player.username
-    val text = args[0]
+    val text = args.joinToString(" ") // Voegt alle woorden samen met een spatie ertussen
+
+    // Stuur het yell-bericht naar alle spelers
     player.world.players.forEach {
-        it.message("${color}[${rank}]${name}:${text}", ChatMessageType.ENGINE)
+        it.message("${color}[${rank}] ${name}: ${text}", ChatMessageType.CONSOLE)
     }
 }
-
