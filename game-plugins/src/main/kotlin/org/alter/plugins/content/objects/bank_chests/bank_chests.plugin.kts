3a42b7d1973076912b37dc89c4f001ff2be9d56f
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-04 18:57:56 +0100

    Create bank_chests.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/objects/bank_chests/bank_chests.plugin.kts
@@ -0,0 +1,24 @@
+package org.alter.plugins.content.objects.bank_chests
+
+import org.alter.plugins.content.interfaces.bank.openBank
+
+private val CHESTS = with(Objs) { setOf(
+         BANK_CHEST_28861
+) }
+
+CHESTS.forEach { booth ->
+    on_obj_option(obj = booth, option = "use") {
+        player.openBank()
+    }
+    if (objHasOption(booth, "Collect")) {
+        on_obj_option(obj = booth, option = "Collect") {
+            open_collect(player)
+        }
+    }
+}
+
+fun open_collect(p: Player) {
+    p.setInterfaceUnderlay(color = -1, transparency = -1)
+    p.openInterface(interfaceId = 402, dest = InterfaceDestination.MAIN_SCREEN)
+}
+
