f0eed24548bdc2196e0ec054a5e4e20ac365b4f2
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-04 11:14:07 +0100

    Set rights command

--- game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/admin/setrights.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/admin/setrights.plugin.kts
@@ -1,9 +1,10 @@
+import org.alter.game.model.priv.Privilege
 import org.alter.plugins.content.commands.Commands_plugin.Command.tryWithUsage
 
 /**
  *   @Author Cl0ud
  */
-on_command("setrights") {
+on_command("setrights", Privilege.ADMIN_POWER) {
     val args = player.getCommandArgs()
     tryWithUsage(player, args, "Cba") { values ->
         val privilege = values[0].toInt()
