511f18a6e73d9d3d70f3a78e6a45f38a063859b3
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-04 11:35:49 +0100

    Update starter_kit.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/mechanics/starter/starter_kit.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/mechanics/starter/starter_kit.plugin.kts
@@ -5,9 +5,6 @@ import org.alter.game.model.attr.NEW_ACCOUNT_ATTR
 on_login {
     if (player.attr[NEW_ACCOUNT_ATTR] ?: return@on_login) {
         with (player.inventory) {
-            add(Items.BREAD, 5)
-            add(Items.BRONZE_PICKAXE)
-            add(Items.BRONZE_AXE)
             add(Items.TRAINING_SWORD)
             add(Items.TRAINING_SHIELD)
             add(Items.TRAINING_BOW)
