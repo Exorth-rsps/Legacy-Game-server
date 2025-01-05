72a16cfc70a5119d3a6432d6fff54929aa308202
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-05 15:06:49 +0100

    Update hunding.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/mainlands/chat/hunding.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/mainlands/chat/hunding.plugin.kts
@@ -6,12 +6,12 @@ on_npc_option(npc = barbarianLadderNpcId, option = "talk-to") { player.queue { b
 suspend fun QueueTask.barbarianDungeonDialogue() {
     chatNpc("Halt, outsider. What brings you to this ladder?")
     chatPlayer("I was curious. What's down there?")
-    chatNpc("Deep below lies a moss-covered monster, a creature of great strength.")
+    chatNpc("Deep below lies a moss-covered monster, <br>a creature of great strength.")
     chatPlayer("That sounds dangerous. Should I be worried?")
-    chatNpc("Not unless you're foolish enough to go unprepared. The beast is safely locked away.You'll need a Mossy Key to open the gate and face it.")
+    chatNpc("Not unless you're foolish enough to go unprepared. <br>The beast is safely locked away.")
     chatNpc("You'll need a Mossy Key to open the gate and face it.")
     chatPlayer("Where can I find this Mossy Key?")
-    chatNpc("The key is rare, found only by those brave enough to defeat creatures in the wild.")
+    chatNpc("The key is rare, <br>found only by those brave enough to defeat creatures in the wild.")
     chatPlayer("Thanks for the warning. I'll make sure to be prepared.")
-    chatNpc("Good. Remember, strength and courage are the only things that matter down there.")
+    chatNpc("Good. Remember, <br>strength and courage are the only things that matter down there.")
 }
