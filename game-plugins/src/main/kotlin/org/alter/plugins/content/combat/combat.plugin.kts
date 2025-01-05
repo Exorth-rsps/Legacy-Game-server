6fd252be16ada5a52184f5e77bba8083373fa76b
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-05 14:48:31 +0100

    quikfix

--- game-plugins/src/main/kotlin/org/alter/plugins/content/combat/combat.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/combat/combat.plugin.kts
@@ -107,7 +107,7 @@ suspend fun cycle(it: QueueTask): Boolean {
                 }
             } else {
                 if (pawn is Player) {
-                    pawn.message("There is no config for this npc, please report.")
+                    //pawn.message("There is no config for this npc, please report.")
                 }
                 strategy.attack(pawn, target)
                 Combat.postAttack(pawn, target)
