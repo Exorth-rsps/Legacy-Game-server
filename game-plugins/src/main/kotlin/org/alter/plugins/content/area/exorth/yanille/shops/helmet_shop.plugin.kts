9e99f07b2a3a4d73a7c5c3b2f9bcd4c2c9a11305
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-05 14:20:01 +0100

    Update helmet_shop.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/shops/helmet_shop.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/shops/helmet_shop.plugin.kts
@@ -4,13 +4,13 @@ import org.alter.game.model.shop.PurchasePolicy
 import org.alter.plugins.content.mechanics.shops.CoinCurrency
 
 create_shop("Helmet Shop", CoinCurrency(), purchasePolicy = PurchasePolicy.BUY_STOCK) {
-    items[0] = ShopItem(Items.BRONZE_MED_HELM, 10, 36, 18, 1, 100)
-    items[1] = ShopItem(Items.IRON_MED_HELM, 10, 132, 66, 1, 200)
-    items[2] = ShopItem(Items.STEEL_MED_HELM, 10, 468, 234, 1, 300)
-    items[3] = ShopItem(Items.BLACK_MED_HELM, 10, 900, 450, 1, 350)
-    items[4] = ShopItem(Items.MITHRIL_MED_HELM, 10, 1300, 650, 1, 400)
-    items[5] = ShopItem(Items.BRONZE_FULL_HELM, 10, 72, 36, 1, 100)
-    items[6] = ShopItem(Items.IRON_FULL_HELM, 10, 264, 132, 1, 200)
-    items[7] = ShopItem(Items.STEEL_FULL_HELM, 10, 936, 468, 1, 300)
+    items[0] = ShopItem(Items.BRONZE_MED_HELM, 5, 36, 18, 1, 100)
+    items[1] = ShopItem(Items.IRON_MED_HELM, 3, 132, 66, 1, 200)
+    items[2] = ShopItem(Items.STEEL_MED_HELM, 2, 468, 234, 1, 300)
+    items[3] = ShopItem(Items.BLACK_MED_HELM, 1, 900, 450, 1, 350)
+    items[4] = ShopItem(Items.MITHRIL_MED_HELM, 1, 1300, 650, 1, 400)
+    items[5] = ShopItem(Items.BRONZE_FULL_HELM, 4, 72, 36, 1, 100)
+    items[6] = ShopItem(Items.IRON_FULL_HELM, 3, 264, 132, 1, 200)
+    items[7] = ShopItem(Items.STEEL_FULL_HELM, 2, 936, 468, 1, 300)
     items[8] = ShopItem(Items.BLACK_FULL_HELM, 10, 1500, 750, 1, 400)
 }
