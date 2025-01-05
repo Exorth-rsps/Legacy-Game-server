b1b1fa835e6c9e8aacd02188762489c005f7f88e
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-05 14:04:01 +0100

    Create louies_armourd_legs.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/shops/louies_armourd_legs.plugin.kts
@@ -0,0 +1,13 @@
+package org.alter.plugins.content.area.varrock.shops
+
+import org.alter.game.model.shop.PurchasePolicy
+import org.alter.plugins.content.mechanics.shops.CoinCurrency
+
+create_shop("Louie's Armoured Legs", CoinCurrency(), purchasePolicy = PurchasePolicy.BUY_STOCK) {
+    items[0] = ShopItem(Items.BRONZE_PLATELEGS, 10, 80, 40, 1, 100)
+    items[1] = ShopItem(Items.IRON_PLATELEGS, 5, 300, 150, 1, 200)
+    items[2] = ShopItem(Items.STEEL_PLATELEGS, 2, 1125, 562, 1, 300)
+    items[3] = ShopItem(Items.BRONZE_PLATESKIRT, 10, 80, 40, 1, 100)
+    items[4] = ShopItem(Items.IRON_PLATESKIRT, 5, 300, 150, 1, 200)
+    items[5] = ShopItem(Items.STEEL_PLATESKIRT, 2, 1125, 562, 1, 300)
+}
