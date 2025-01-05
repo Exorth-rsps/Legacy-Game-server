3ccdee3ef84bed8ac022f89dbfed98e2a01e80c7
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-05 14:20:35 +0100

    Update horviks_amour_shop.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/shops/horviks_amour_shop.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/shops/horviks_amour_shop.plugin.kts
@@ -5,9 +5,9 @@ import org.alter.plugins.content.mechanics.shops.CoinCurrency
 
 create_shop("Horvik's Armour Shop", CoinCurrency(), purchasePolicy = PurchasePolicy.BUY_STOCK) {
     items[0] = ShopItem(Items.BRONZE_CHAINBODY, 10, 60, 30, 1, 100)
-    items[1] = ShopItem(Items.IRON_CHAINBODY, 10, 210, 105, 1, 200)
-    items[2] = ShopItem(Items.STEEL_CHAINBODY, 10, 750, 375, 1, 300)
-    items[3] = ShopItem(Items.BRONZE_PLATEBODY, 10, 160, 80, 1, 100)
-    items[4] = ShopItem(Items.IRON_PLATEBODY, 10, 560, 280, 1, 200)
-    items[5] = ShopItem(Items.STEEL_PLATEBODY, 10, 2000, 1000, 1, 300)
+    items[1] = ShopItem(Items.IRON_CHAINBODY, 5, 210, 105, 1, 200)
+    items[2] = ShopItem(Items.STEEL_CHAINBODY, 1, 750, 375, 1, 300)
+    items[3] = ShopItem(Items.BRONZE_PLATEBODY, 8, 160, 80, 1, 100)
+    items[4] = ShopItem(Items.IRON_PLATEBODY, 6, 560, 280, 1, 200)
+    items[5] = ShopItem(Items.STEEL_PLATEBODY, 1, 2000, 1000, 1, 300)
 }
