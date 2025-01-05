eee9c3330ce56f0af449e16da563c45b0db82dad
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-05 14:08:20 +0100

    Create cassies_shield_shop.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/shops/cassies_shield_shop.plugin.kts
@@ -0,0 +1,16 @@
+package org.alter.plugins.content.area.varrock.shops
+
+import org.alter.game.model.shop.PurchasePolicy
+import org.alter.plugins.content.mechanics.shops.CoinCurrency
+
+create_shop("Cassie's Shield Shop", CoinCurrency(), purchasePolicy = PurchasePolicy.BUY_STOCK) {
+    // Wooden Shield
+    items[0] = ShopItem(Items.WOODEN_SHIELD, 5, 10, 5, 1, 50)
+    items[1] = ShopItem(Items.BRONZE_SQ_SHIELD, 4, 48, 24, 1, 100)
+    items[2] = ShopItem(Items.IRON_SQ_SHIELD, 3, 172, 86, 1, 200)
+    items[3] = ShopItem(Items.STEEL_SQ_SHIELD, 2, 600, 300, 1, 300)
+    items[4] = ShopItem(Items.BLACK_SQ_SHIELD, 1, 1200, 600, 1, 400)
+    items[5] = ShopItem(Items.BRONZE_KITESHIELD, 3, 64, 32, 1, 100)
+    items[6] = ShopItem(Items.IRON_KITESHIELD, 2, 224, 112, 1, 200)
+    items[7] = ShopItem(Items.STEEL_KITESHIELD, 1, 1200, 400, 1, 300)
+}
