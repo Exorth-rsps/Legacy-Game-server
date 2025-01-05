2d3c6dfe6f0a774ca6e20e5252724af4ce59abbd
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-05 12:22:57 +0100

    Create yanille_general_store.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/shops/yanille_general_store.plugin.kts
@@ -0,0 +1,17 @@
+package org.alter.plugins.content.area.exorth.yanille.shops
+
+import org.alter.game.model.shop.PurchasePolicy
+import org.alter.plugins.content.mechanics.shops.CoinCurrency
+
+create_shop("Yanille General Store", CoinCurrency(), purchasePolicy = PurchasePolicy.BUY_TRADEABLES) {
+    items[0] = ShopItem(Items.TINDERBOX, 10, 3, 1)
+    items[1] = ShopItem(Items.HAMMER, 7, 3, 1)
+    items[2] = ShopItem(Items.KNIFE, 6, 4, 1)
+    items[3] = ShopItem(Items.CHISEL, 3, 5, 1)
+    items[4] = ShopItem(Items.SHEARS, 2, 9, 1)
+    items[5] = ShopItem(Items.BRONZE_AXE, 2, 25, 1)
+    items[6] = ShopItem(Items.BRONZE_PICKAXE, 2, 25, 1)
+    items[7] = ShopItem(Items.SPADE, 2, 9, 1)
+    items[8] = ShopItem(Items.BUCKET, 2, 4, 1)
+    items[9] = ShopItem(Items.EMPTY_VIAL, 21, 3, 1)
+}
