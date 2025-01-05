b0a6e26aafb79156dca00cf3aef6ad4b4dd131bc
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-05 12:32:40 +0100

    Create frenitas_cooked_food_shop.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/shops/frenitas_cooked_food_shop.plugin.kts
@@ -0,0 +1,14 @@
+package org.alter.plugins.content.area.exorth.yanille.shops
+
+import org.alter.game.model.shop.PurchasePolicy
+import org.alter.plugins.content.mechanics.shops.CoinCurrency
+
+create_shop("Frenita's Cooked Food Shop", CoinCurrency(), purchasePolicy = PurchasePolicy.BUY_STOCK) {
+    items[0] = ShopItem(Items.COOKED_MEAT, 15, 5, 1, 1, 100)
+    items[1] = ShopItem(Items.COOKED_CHICKEN, 15, 5, 1, 1, 100)
+    items[2] = ShopItem(Items.BREAD, 10, 7, 5, 1, 100)
+    items[3] = ShopItem(Items.BAKED_POTATO, 5, 12, 1, 1, 100)
+    items[4] = ShopItem(Items.PLAIN_PIZZA, 2, 53, 1,1,100)
+    items[5] = ShopItem(Items.SHRIMPS, 23, 7, 1,1,100)
+    items[6] = ShopItem(Items.HERRING, 12, 27, 1,1,100)
+}
