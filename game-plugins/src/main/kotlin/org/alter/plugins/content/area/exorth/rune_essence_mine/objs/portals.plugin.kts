49145b4f2a64262ce66faf7bc8dc5487068e0289
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-06 21:50:18 +0100

    Add rune essence return portal

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/rune_essence_mine/objs/portals.plugin.kts
@@ -0,0 +1,12 @@
+package gg.rsmod.plugins.content.areas.rune_essence_mine.objs
+/**
+ * @author Eikenb00m <https://github.com/eikenb00m>
+ */
+
+
+on_obj_option(obj = Objs.PORTAL_34774, option = "Use") {
+    player.moveTo(2679, 1637, 0)
+}
+
+
+
