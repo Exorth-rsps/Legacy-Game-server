688ab9c2c55b4a72550882ec60b2f802eaaaa20c
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-09 20:21:11 +0100

    Create spawns.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/rune_essence_mine/spawns/spawns.plugin.kts
@@ -0,0 +1,24 @@
+package org.alter.plugins.content.area.exorth.rune_essence_mine.spawns
+
+spawn_npc(Npcs.ABYSSAL_LEECH, 3589, 1242, 0, 3, Direction.EAST)
+spawn_npc(Npcs.ABYSSAL_LEECH, 3591, 1244, 0, 3, Direction.EAST)
+spawn_npc(Npcs.ABYSSAL_LEECH, 3594, 1243, 0, 3, Direction.EAST)
+spawn_npc(Npcs.ABYSSAL_LEECH, 3596, 1246, 0, 3, Direction.EAST)
+spawn_npc(Npcs.ABYSSAL_LEECH, 3599, 1244, 0, 3, Direction.EAST)
+spawn_npc(Npcs.ABYSSAL_LEECH, 3601, 1246, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.ABYSSAL_GUARDIAN, 3627, 1251, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_GUARDIAN, 3630, 1256, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_GUARDIAN, 3628, 1260, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_WALKER, 3632, 1261, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_GUARDIAN, 3632, 1257, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_GUARDIAN, 3632, 1252, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_WALKER, 3635, 1251, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_GUARDIAN, 3636, 1254, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_GUARDIAN, 3636, 1257, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_GUARDIAN, 3636, 1261, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_WALKER, 3639, 1260, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_GUARDIAN, 3639, 1254, 0, 3, Direction.WEST)
+spawn_npc(Npcs.ABYSSAL_WALKER, 3641, 1252, 0, 3, Direction.WEST)
+
+
