6279b09791cfe6fb1c2759e9a242133bcd87b6e2
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-09 19:28:39 +0100

    Yanille dungeon update

--- game-plugins/src/main/kotlin/org/alter/plugins/content/objects/ladder/special_ladders.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/objects/ladder/special_ladders.plugin.kts
@@ -22,4 +22,20 @@ on_obj_option(obj = Objs.TRAPDOOR_26119, option = "Climb-Down") {
         else -> player.message("Nothing interesting happens.")
     }
 }
+on_obj_option(obj = Objs.LADDER_17384, option = "Climb-Down") {
+    when(player.tile.regionId) {
+        10521 -> { //Wizardsguild
+            player.moveTo(x = 3766, z = 1217, height = 0)
+        }
+        else -> player.message("Nothing interesting happens.")
+    }
+}
+on_obj_option(obj = Objs.LADDER_17385, option = "Climb-Up") {
+    when(player.tile.regionId) {
+        14867 -> { //Wizardsguild
+            player.moveTo(x = 2683, z = 1636, height = 0)
+        }
 
+        else -> player.message("Nothing interesting happens.")
+    }
+}
