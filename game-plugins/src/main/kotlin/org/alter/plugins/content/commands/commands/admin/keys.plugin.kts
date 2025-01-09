0557e22e57c5de3ddd76251a26a0532a9195a7af
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-09 21:02:05 +0100

    add extra keys to command

--- game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/admin/keys.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/admin/keys.plugin.kts
@@ -1,8 +1,10 @@
 import org.alter.game.model.priv.Privilege
 
-on_command("keys", Privilege.ADMIN_POWER, description = "Gives you the three keys form key dungeon.") {
+on_command("keys", Privilege.ADMIN_POWER, description = "Gives all keys") {
     player.inventory.add(item = Items.KEY_1543, amount = 1)
     player.inventory.add(item = Items.KEY_1544, amount = 1)
     player.inventory.add(item = Items.KEY_1545, amount = 1)
+    player.inventory.add(item = Items.GIANT_KEY, amount = 1)
+    player.inventory.add(item = Items.MOSSY_KEY, amount = 1)
 
 }
