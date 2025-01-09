eb9dd8128453f895f4469d3921cae5a9ae2780d4
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-09 20:42:15 +0100

    Create fishingspots.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/yanille/spawns/fishingspots.plugin.kts
@@ -0,0 +1,4 @@
+package org.alter.plugins.content.area.exorth.yanille.spawns
+
+spawn_npc(Npcs.FISHING_SPOT_3317, 2632, 1629, 0, 0, Direction.WEST)
+spawn_npc(Npcs.FISHING_SPOT_3317, 2633, 1629, 0, 0, Direction.WEST)
