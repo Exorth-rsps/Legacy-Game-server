a3d527910d9ecffea43480273370573abee677c9
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-14 20:47:53 +0100

    Update seers_dungeon.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/seers/dungeon/seers_dungeon.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/seers/dungeon/seers_dungeon.plugin.kts
@@ -1,10 +1,5 @@
 package gg.rsmod.plugins.content.areas.exorth.seers.dungeon
 
-import org.alter.api.InterfaceDestination
-import org.alter.api.cfg.Items
-import org.alter.api.cfg.Objs
-import org.alter.api.ext.openInterface
-
 /**
  * @author Eikenb00m <https://github.com/eikenb00m>
  */
@@ -38,25 +33,97 @@ on_obj_option(obj = Objs.ODD_LOOKING_WALL_26114, option = "Open", lineOfSightDis
 }
 
 on_obj_option(obj = Objs.GATE_29486, option = "Open", lineOfSightDistance = 1) {
+    if (!player.inventory.contains(Items.GIANT_KEY)) {
+        player.message("You need a key to open this gate.")
+        return@on_obj_option
+    }
+
     player.queue {
-        player.openInterface(interfaceId = 97, dest = InterfaceDestination.MAIN_SCREEN)
-        player.message("Interface 97")
-        wait(10)
-        player.closeInterface (interfaceId =97)
-        player.message("Close Interface 97")
-        wait(10)
-        player.openInterface(interfaceId = 98, dest = InterfaceDestination.MAIN_SCREEN)
-        player.message("Interface 98")
-        wait(10)
-        player.openInterface(interfaceId = 96, dest = InterfaceDestination.MAIN_SCREEN)
-        player.message("Interface 96")
-        wait(10)
-        player.openInterface(interfaceId = 110, dest = InterfaceDestination.MAIN_SCREEN)
-        player.message("Interface 110")
-        wait(3)
+        player.inventory.remove(Items.GIANT_KEY, 1)
+        val x = 4178
+        val z = 1239
+        player.moveTo(tile = Tile(x = x, z = z))
+    }
+}
+on_obj_option(obj = Objs.GATE_29487, option = "Open", lineOfSightDistance = 1) {
+    if (!player.inventory.contains(Items.GIANT_KEY)) {
+        player.message("You need a key to open this gate.")
+        return@on_obj_option
+    }
+
+    player.queue {
+        player.inventory.remove(Items.GIANT_KEY, 1)
+        val x = 4179
+        val z = 1239
+        player.moveTo(tile = Tile(x = x, z = z))
+    }
+}
+on_obj_option(obj = Objs.GATE_29489, option = "Open", lineOfSightDistance = 1) {
+    player.queue {
+        val x = 4182
+        val z = 1257
+        player.moveTo(tile = Tile(x = x, z = z))
+    }
+}
+on_obj_option(obj = Objs.GATE_29488, option = "Open", lineOfSightDistance = 1) {
+
+    player.queue {
+        val x = 4182
+        val z = 1256
+        player.moveTo(tile = Tile(x = x, z = z))
+    }
+}
+//TO-DO: Facing issues
+on_obj_option(obj = Objs.ROCKS_29491, option = 1) {
+    val obj = player.getInteractingGameObj()
+    val isNorth = player.tile.z > obj.tile.z
+    val offsetZ = if (isNorth) -1 else 1
+    val StartTile = Tile(obj.tile.x, obj.tile.z)
+    val EndTile = Tile(obj.tile.x, obj.tile.z + 3 * offsetZ)
+
+    player.lockingQueue {
+        if (player.tile != StartTile) {
+            player.faceDirection(Direction.NORTH)
+            val distance = player.tile.getDistance(StartTile)
+            player.walkTo(StartTile)
+            wait(distance + 2)
+        } else {
+            player.faceDirection(Direction.NORTH)
+            wait(2)
         }
 
+        val faceTile = Tile(obj.tile.x, obj.tile.z + (if (isNorth) -1 else 1))
+        player.faceTile(faceTile)
+        wait(1)
+
+        // Loop for forced movement
+        for (i in 1..2) {
+            player.animate(if (i == 1) 740 else 737)
+
+            val nextTile = Tile(obj.tile.x, obj.tile.z + i * offsetZ)
+
+            player.queue {
+                player.faceDirection(Direction.NORTH)
+                val move = ForcedMovement.of(
+                    player.tile,
+                    nextTile,
+                    clientDuration1 = if (i == 1) 25 else 25,
+                    clientDuration2 = 60,
+                    directionAngle = if (isNorth) Direction.NORTH.ordinal else Direction.SOUTH.ordinal,
+                    lockState = LockState.NONE
+                )
+                player.faceDirection(Direction.NORTH)
+                player.forceMove(this, move)
+            }
+            wait(1)
+        }
+        player.animate(737)
+        waitTile(EndTile)
+        player.animate(738)
     }
+}
+
+
 
 
 
