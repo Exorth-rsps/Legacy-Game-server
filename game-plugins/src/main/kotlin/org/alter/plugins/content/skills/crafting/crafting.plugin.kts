a020c85036b9b96936bf311a6a1d6f3ea2b7eb47
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-01-17 18:39:28 +0100

    Update crafting.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/skills/crafting/crafting.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/skills/crafting/crafting.plugin.kts
@@ -4,6 +4,10 @@ import org.alter.api.cfg.Items
 import org.alter.plugins.content.skills.crafting.data.Gems
 import org.alter.plugins.content.skills.crafting.data.Spin
 
+val spinningObjs = setOf(
+    Objs.SPINNING_WHEEL_25824,
+    Objs.SPINNING_WHEEL_8748
+)
 val spinningObj = 25824
 
 Gems.values().forEach { gems ->
@@ -11,8 +15,10 @@ Gems.values().forEach { gems ->
         player.queue { Cutting.gemCut(this, gems) }
     }
 }
-Spin.values().forEach { spin ->
-    on_item_on_obj(obj = spinningObj, item = spin.unSpun) {
-        player.queue { Spinning.Spin(this, spin) }
+spinningObjs.forEach { spinningObj ->
+    Spin.values().forEach { spin ->
+        on_item_on_obj(obj = spinningObj, item = spin.unSpun) {
+            player.queue { Spinning.Spin(this, spin) }
+        }
     }
 }
