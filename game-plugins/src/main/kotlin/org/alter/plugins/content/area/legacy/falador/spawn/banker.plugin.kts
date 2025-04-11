088de849321ebf6657d09a7e5367e65748a83eda
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-11 21:16:12 +0200

    Spawn Falador Bankers

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/falador/spawn/banker.plugin.kts
@@ -0,0 +1,14 @@
+package org.alter.plugins.content.area.legacy.falador.spawn
+
+spawn_npc(Npcs.BANKER_1613, 2945, 3366, 0, 0, Direction.NORTH)
+spawn_npc(Npcs.BANKER_1634, 2946, 3366, 0, 0, Direction.NORTH)
+spawn_npc(Npcs.BANKER_1613, 2947, 3366, 0, 0, Direction.NORTH)
+spawn_npc(Npcs.BANKER_1634, 2948, 3366, 0, 0, Direction.NORTH)
+spawn_npc(Npcs.BANKER_1613, 2949, 3366, 0, 0, Direction.NORTH)
+
+spawn_npc(Npcs.BANKER_1613, 3010, 3353, 0, 0, Direction.NORTH)
+spawn_npc(Npcs.BANKER_1634, 3011, 3353, 0, 0, Direction.NORTH)
+spawn_npc(Npcs.BANKER_1613, 3012, 3353, 0, 0, Direction.NORTH)
+spawn_npc(Npcs.BANKER_1634, 3013, 3353, 0, 0, Direction.NORTH)
+spawn_npc(Npcs.BANKER_1613, 3014, 3353, 0, 0, Direction.NORTH)
+spawn_npc(Npcs.BANKER_1613, 3015, 3353, 0, 0, Direction.NORTH)
