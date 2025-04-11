a1fcc02eaa0f73005dce2be32dac98213fb38a1d
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-11 21:15:55 +0200

    Moved Sedridor

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/rune_essence_mine/objs/portals.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/exorth/rune_essence_mine/objs/portals.plugin.kts
@@ -5,7 +5,7 @@ package gg.rsmod.plugins.content.areas.rune_essence_mine.objs
 
 
 on_obj_option(obj = Objs.PORTAL_34774, option = "Use") {
-    player.moveTo(2679, 1637, 0)
+    player.moveTo(2967, 3415, 0)
 }
 
 
--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/falador/chat/archmage_sedridor.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/falador/chat/archmage_sedridor.plugin.kts
@@ -15,10 +15,6 @@ suspend fun QueueTask.dialog() {
                 continueDialog = false
             }
             2 -> {
-                question1()
-                continueDialog = true
-            }
-            3 -> {
                 no_thank_you()
                 continueDialog = false
             }
@@ -28,24 +24,12 @@ suspend fun QueueTask.dialog() {
 
 suspend fun QueueTask.options(): Int = options(
     "Can you teleport me to the Rune Essence Mine?",
-    "What is this place?",
     "No thank you."
 )
 
 suspend fun QueueTask.no_thank_you() {
     chatPlayer("No thank you.", animation = 568)
     chatNpc("Well, have a good day.", animation = 554)
-}
-suspend fun QueueTask.question1() {
-    chatPlayer("What is this place?", animation = 568)
-    chatNpc("Welcome to the Magic Guild.<br>A place where allot of magic happens.", animation = 554)
-    chatPlayer("Oh wow, Magic!<br>What can i find here?", animation = 568)
-    chatNpc("In the basement you can find some zombies locked in a cage!", animation = 554)
-    chatNpc("Here at the ground floor you find me...<br> I can send you to the rune essence mines.", animation = 554)
-    chatNpc("At the first floor, you find Wizard Akutha.<br>He sells some Magic Supplies in his Magic Guild Store.", animation = 554)
-    chatNpc("At the top you find Zavistic Rave, he is doing something with portal teleportation...", animation = 554)
-    chatPlayer("Well im gonna check out those things!", animation = 568)
-
 }
 
 suspend fun QueueTask.teleport_me() {
