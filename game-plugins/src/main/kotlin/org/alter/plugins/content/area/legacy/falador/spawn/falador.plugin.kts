94e58b487b177917df6460d24d4dec46c713427a
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-11 22:28:30 +0200

    Fally spawns

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/falador/spawn/falador.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/falador/spawn/falador.plugin.kts
@@ -1,3 +1,51 @@
 package org.alter.plugins.content.area.legacy.falador.spawn
 
 spawn_npc(Npcs.ARCHMAGE_SEDRIDOR_11433, 2967, 3414, 0, 5, Direction.SOUTH)
+
+spawn_npc(Npcs.GUARD_3269, 2963, 3397, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.GUARD_3269, 2967, 3397, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.GUARD_3269, 2964, 3394, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.GUARD_3269, 2967, 3394, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.GUARD_3269, 2963, 3391, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.GUARD_3269, 2967, 3391, 0, 5, Direction.NORTH)
+
+spawn_npc(Npcs.COW, 2976, 3407, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.COW, 2976, 3404, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.COW, 2979, 3402, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.COW, 2980, 3406, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.COW, 2984, 3406, 0, 3, Direction.NORTH)
+spawn_npc(Npcs.COW, 2984, 3402, 0, 3, Direction.NORTH)
+
+spawn_npc(Npcs.GIANT_RAT_2858, 2968, 3375, 0, 3, Direction.NORTH_EAST)
+spawn_npc(Npcs.GIANT_RAT_2858, 2968, 3372, 0, 3, Direction.NORTH_EAST)
+spawn_npc(Npcs.GIANT_RAT_2858, 2968, 3369, 0, 3, Direction.NORTH_EAST)
+spawn_npc(Npcs.GIANT_RAT_2858, 2963, 3375, 0, 3, Direction.NORTH_WEST)
+spawn_npc(Npcs.GIANT_RAT_2858, 2963, 3372, 0, 3, Direction.NORTH_WEST)
+spawn_npc(Npcs.GIANT_RAT_2858, 2963, 3369, 0, 3, Direction.NORTH_WEST)
+
+spawn_npc(Npcs.SHOP_KEEPER, 2959, 3389, 0, 3, Direction.SOUTH)
+spawn_npc(Npcs.SHOP_ASSISTANT, 2956, 3389, 0, 3, Direction.SOUTH)
+
+spawn_npc(Npcs.HILL_GIANT, 2986, 3387, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.HILL_GIANT_2099, 2986, 3383, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.HILL_GIANT_2100, 2990, 3379, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.HILL_GIANT_2101, 2991, 3388, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.HILL_GIANT_2102, 2994, 3387, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.HILL_GIANT_2103, 2994, 3380, 0, 5, Direction.SOUTH)
+
+spawn_npc(Npcs.MOSS_GIANT, 3009, 3384, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT_2091, 3014, 3385, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT_2092, 3011, 3378, 0, 5, Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT_2093, 3014, 3373, 0, 5, Direction.NORTH)
+
+spawn_npc(Npcs.GUARD_3269, 3008, 3325, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.GUARD_3269, 3005, 3325, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.GUARD_3269, 3005, 3321, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.GUARD_3269, 3008, 3321, 0, 5, Direction.SOUTH)
+
+spawn_npc(Npcs.CHICKEN_1173, 3025, 3367, 0, 3, Direction.SOUTH)
+spawn_npc(Npcs.CHICKEN_1174, 3024, 3365, 0, 3, Direction.SOUTH)
+spawn_npc(Npcs.CHICKEN_1173, 3026, 3363, 0, 3, Direction.SOUTH)
+spawn_npc(Npcs.CHICKEN_1174, 3029, 3363, 0, 3, Direction.SOUTH)
+spawn_npc(Npcs.CHICKEN_1173, 3029, 3366, 0, 3, Direction.SOUTH)
+spawn_npc(Npcs.CHICKEN_1174, 3027, 3368, 0, 3, Direction.SOUTH)
