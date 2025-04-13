55e7f9a08cebf6285f1085e0e4ee9fae4bf5d0fb
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-13 20:03:36 +0200

    Update fishing_guild.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/fishing_guild/spawn/fishing_guild.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/fishing_guild/spawn/fishing_guild.plugin.kts
@@ -2,3 +2,13 @@ package org.alter.plugins.content.area.legacy.fishing_guild.spawn
 
 spawn_npc(Npcs.HARRY, 2597, 3400, 0, 3, Direction.NORTH)
 
+spawn_npc(Npcs.BANKER_1613, 2584, 3418, 0, 0, Direction.EAST)
+spawn_npc(Npcs.BANKER_1634, 2584, 3419, 0, 0, Direction.EAST)
+spawn_npc(Npcs.BANKER_1613, 2584, 3421, 0, 0, Direction.EAST)
+spawn_npc(Npcs.BANKER_1634, 2584, 3422, 0, 0, Direction.EAST)
+
+spawn_npc(Npcs.FISHING_SPOT_1530, 2599, 3419, 0, 0, Direction.EAST)
+spawn_npc(Npcs.ROD_FISHING_SPOT_1526, 2603, 3419, 0, 0, Direction.EAST)
+spawn_npc(Npcs.FISHING_SPOT_1511, 2605, 3421, 0, 0, Direction.EAST)
+spawn_npc(Npcs.FISHING_SPOT_1522, 2598, 3425, 0, 0, Direction.EAST)
+spawn_npc(Npcs.FISHING_SPOT_5820, 2605, 3425, 0, 0, Direction.EAST)
