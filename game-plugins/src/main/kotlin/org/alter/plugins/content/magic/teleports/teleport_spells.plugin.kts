c5fe67cf6053fd5c6d584d87fb5e1563fd14a666
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-13 09:31:28 +0200

    Remove teleport spell requirements

--- game-plugins/src/main/kotlin/org/alter/plugins/content/magic/teleports/teleport_spells.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/magic/teleports/teleport_spells.plugin.kts
@@ -20,14 +20,14 @@ on_magic_spell_button("Respawn Teleport") { metadata ->
 fun Player.teleport(spell: TeleportSpell, data: SpellMetadata) = teleport(spell.type, spell.endArea.randomTile, spell.xp, data)
 
 fun Player.teleport(type: TeleportType, endTile: Tile, xp: Double, data: SpellMetadata) {
-    if (!MagicSpells.canCast(this, data.lvl, data.items, data.spellbook)) {
-        return
-    }
+//    if (!MagicSpells.canCast(this, data.lvl, data.items, data.spellbook)) {
+//        return
+//    }
 
     if (canTeleport(type)) {
-        MagicSpells.removeRunes(this, data.items)
+        //MagicSpells.removeRunes(this, data.items)
         teleport(endTile, type)
-        addXp(Skills.MAGIC, xp)
+        //addXp(Skills.MAGIC, xp)
         world.spawn(AreaSound(tile, SOUNDAREA_ID, SOUNDAREA_RADIUS, SOUNDAREA_VOLUME))
     }
 }
