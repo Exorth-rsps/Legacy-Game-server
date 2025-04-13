2d83de0939cafe03c872eb5c623ab609e9d36504
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-13 21:10:17 +0200

    Add all objects and shortcuts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/asgarnian_ice_dungeon/objs/objects.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/asgarnian_ice_dungeon/objs/objects.plugin.kts
@@ -14,3 +14,45 @@ on_obj_option(obj = Objs.LADDER, option = "climb-down", lineOfSightDistance = 1)
             }
     }
 }
+on_obj_option(obj = Objs.TUNNEL_42506, option = "Enter") {
+    if (!player.inventory.contains(Items.ICY_KEY) || player.getSkills().getCurrentLevel(Skills.AGILITY) < 50) {
+        player.message("You need an icy key and at least level 50 Agility to use this tunnel.")
+        return@on_obj_option
+    }
+    player.moveTo(3035, 9557, player.tile.height)
+}
+on_obj_option(obj = Objs.TUNNEL_42507, option = "Enter") {
+    if (!player.inventory.contains(Items.ICY_KEY) || player.getSkills().getCurrentLevel(Skills.AGILITY) < 50) {
+        player.message("You need an icy key and at least level 50 Agility to use this tunnel.")
+        return@on_obj_option
+    }
+    player.moveTo(3026, 9572, player.tile.height)
+}
+on_obj_option(obj = Objs.ICY_CAVERN_10596, option = "Enter") {
+    if (!player.inventory.contains(Items.ICY_KEY)) {
+        player.message("You need an icy key to use this tunnel.")
+        return@on_obj_option
+    }
+    player.moveTo(3056, 9555, player.tile.height)
+}
+on_obj_option(obj = Objs.ICY_CAVERN, option = "Enter") {
+    if (!player.inventory.contains(Items.ICY_KEY)) {
+        player.message("You need an icy key to use this tunnel.")
+        return@on_obj_option
+    }
+    player.moveTo(3056, 9562, player.tile.height)
+}
+
+on_obj_option(obj = Objs.STEPS, option = "Climb") {
+    if (player.tile.x == 3060 && player.tile.z == 9555) {
+        if (!player.inventory.contains(Items.FROZEN_KEY)) {
+            player.message("You need a frozen key to open this wall.")
+            return@on_obj_option
+        }
+        player.moveTo(3060, 9557, player.tile.height)
+    } else if (player.tile.x == 3060 && player.tile.z == 9557) {
+        player.moveTo(3060, 9555, player.tile.height)
+    } else {
+        player.message("Nothing interesting happens.")
+    }
+}
