057039af9cd177ad2f10f376bd51e2f3503f966f
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-13 20:47:35 +0200

    Fix the ladder

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/asgarnian_ice_dungeon/objs/objects.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/asgarnian_ice_dungeon/objs/objects.plugin.kts
@@ -6,15 +6,11 @@ package org.alter.plugins.content.area.legacy.asgarnian_ice_dungeon.objs
 on_obj_option(obj = Objs.LADDER, option = "climb-down", lineOfSightDistance = 1) {
     when (player.tile.regionId) {
         11829 -> {
-            on_obj_option(obj = Objs.LADDER, option = "Open", lineOfSightDistance = 1) {
                 if (!player.inventory.contains(Items.COLD_KEY)) {
                     player.message("You need a cold key to use this ladder.")
                     return@on_obj_option
                 }
                 player.moveTo(3007, 9550, player.tile.height)
             }
-
-        }
-
     }
 }
