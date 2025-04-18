42cbb5a1cb8f59602324045e46269d4859c853c6
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-18 22:17:58 +0200

    Create banker.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/draynor/spawn/banker.plugin.kts
@@ -0,0 +1,6 @@
+package org.alter.plugins.content.area.legacy.draynor.spawn
+
+spawn_npc(Npcs.BANKER_1613, 3090, 3242, 0, 0, Direction.EAST)
+spawn_npc(Npcs.BANKER_1634, 3090, 3243, 0, 0, Direction.EAST)
+spawn_npc(Npcs.BANKER_1613, 3090, 3244, 0, 0, Direction.EAST)
+
