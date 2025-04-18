4b4065817e92de6578d7cf05e293713c1694ba20
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-18 22:53:25 +0200

    Create spawn.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/draynor/spawn/spawn.plugin.kts
@@ -0,0 +1,16 @@
+package org.alter.plugins.content.area.legacy.draynor.spawn
+
+spawn_npc(Npcs.FARMER_3243, 3081, 3249, 0, 5, Direction.EAST)
+spawn_npc(Npcs.MASTER_FARMER_5730, 3077, 3252, 0, 5, Direction.EAST)
+spawn_npc(Npcs.WATCHMAN_5420, 3082, 3253, 0, 5, Direction.EAST)
+spawn_npc(Npcs.WATCHMAN_5420, 3077, 3248, 0, 5, Direction.EAST)
+spawn_npc(Npcs.HERO_3295, 3094, 3270, 0, 5, Direction.EAST)
+spawn_npc(Npcs.HERO_3295, 3096, 3272, 0, 5, Direction.EAST)
+spawn_npc(Npcs.PALADIN_3293, 3093, 3260, 0, 5, Direction.EAST)
+spawn_npc(Npcs.PALADIN_3293, 3091, 3258, 0, 5, Direction.EAST)
+spawn_npc(Npcs.TANNER, 3097, 3258, 0, 5, Direction.EAST)
+spawn_npc(Npcs.ROMMIK, 3100, 3260, 0, 5, Direction.EAST)
+spawn_npc(Npcs.BOB, 3086, 3259, 0, 5, Direction.EAST)
+spawn_npc(Npcs.FRIENDLY_FORESTER_11427, 3095, 3239, 0, 0, Direction.SOUTH)
+
+
