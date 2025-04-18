f2b9196bf2c2265df72dc5f0cf336d230964f835
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-18 21:13:36 +0200

    Update CoinCurrency.kt

--- game-plugins/src/main/kotlin/org/alter/plugins/content/mechanics/shops/CoinCurrency.kt
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/mechanics/shops/CoinCurrency.kt
@@ -6,3 +6,4 @@ import org.alter.api.cfg.Items
  * @author Tom <rspsmods@gmail.com>
  */
 class CoinCurrency : ItemCurrency(Items.COINS_995, singularCurrency = "coin", pluralCurrency = "coins")
+class UnidentifiedMineralsCurrency : ItemCurrency(Items.UNIDENTIFIED_MINERALS, singularCurrency = "unidentified mineral", pluralCurrency = "unidentified minerals")
