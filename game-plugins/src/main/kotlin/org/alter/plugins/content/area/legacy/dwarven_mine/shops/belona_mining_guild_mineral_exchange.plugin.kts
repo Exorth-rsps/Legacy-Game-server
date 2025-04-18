e312089caa9246f77de22df1b752b1909ac8f3b5
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-18 22:17:52 +0200

    Update belona_mining_guild_mineral_exchange.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/dwarven_mine/shops/belona_mining_guild_mineral_exchange.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/dwarven_mine/shops/belona_mining_guild_mineral_exchange.plugin.kts
@@ -7,7 +7,7 @@ import org.alter.plugins.content.mechanics.shops.UnidentifiedMineralsCurrency
 create_shop("Mining Guild Mineral Exchange", UnidentifiedMineralsCurrency(), purchasePolicy = PurchasePolicy.BUY_NONE) {
     // Wooden Shield
     items[0] = ShopItem(Items.MINING_GLOVES, 5, 400, 400, 1, 500)
-    items[1] = ShopItem(Items.EXPERT_MINING_GLOVES, 3, 800, 800, 1, 500)
+    items[1] = ShopItem(Items.SUPERIOR_MINING_GLOVES, 3, 800, 800, 1, 500)
     items[2] = ShopItem(Items.EXPERT_MINING_GLOVES, 1, 1200, 1200, 1, 500)
     items[3] = ShopItem(Items.DRAGON_PICKAXE, 1, 2500, 2500, 1, 500)
     items[4] = ShopItem(Items.PROSPECTOR_HELMET, 1, 1200, 1200, 1, 500)
