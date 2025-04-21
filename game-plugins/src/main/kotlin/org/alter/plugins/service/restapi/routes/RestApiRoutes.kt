9b01a1b3c33e518c8ca7b81ccf4ab88368cba40e
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-21 22:25:10 +0200

    Update RestApiRoutes.kt

--- game-plugins/src/main/kotlin/org/alter/plugins/service/restapi/routes/RestApiRoutes.kt
+++ game-plugins/src/main/kotlin/org/alter/plugins/service/restapi/routes/RestApiRoutes.kt
@@ -1,6 +1,9 @@
 package org.alter.plugins.service.restapi.routes
 
 import com.google.gson.Gson
+import java.util.Timer
+import java.util.TimerTask
+import java.util.concurrent.CountDownLatch
 import org.alter.game.model.World
 import org.alter.plugins.service.restapi.controllers.CommandsController
 import org.alter.plugins.service.restapi.controllers.OnlinePlayersController
@@ -13,6 +16,26 @@ class RestApiRoutes {
         get("/players") {
                 req, res -> Gson().toJson(OnlinePlayersController(req, res, false).init(world))
         }
+                get("/players/stream") { req, res ->
+                        res.type("text/event-stream")
+                        res.header("Cache-Control", "no-cache")
+                        val out = res.raw().writer
+                        val gson = Gson()
+                    
+                        Timer(true).scheduleAtFixedRate(object: TimerTask() {
+                                override fun run() {
+                                        val json = gson.toJson(
+                                                OnlinePlayersController(req, res, false).init(world)
+                                                    )
+                                        out.write("data: $json\n\n")
+                                        out.flush()
+                                    }
+                            }, 0, 5000)
+            
+                        // Houd de verbinding open
+                        CountDownLatch(1).await()
+                        ""
+                    }
 
         get("/player/:name") {
                 req, res -> Gson().toJson(PlayerController(req, res, false).init(world))
