a46cd912a31ede13774dffc6163a035da0ab869e
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-21 11:04:25 +0200

    Create rommik.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/draynor/chat/rommik.plugin.kts
@@ -0,0 +1,39 @@
+package org.alter.plugins.content.area.legacy.draynor.chat
+
+arrayOf(Npcs.ROMMIK).forEach { shop ->
+
+    on_npc_option(npc = shop, option = "talk-to", lineOfSightDistance = 1) { player.queue { dialog() } }
+    on_npc_option(npc = shop, option = "trade") { open_shop(player) }
+}
+
+    suspend fun QueueTask.dialog() {
+        chatNpc("Would you like to buy some crafting supplies?")
+        var continueDialog = true
+        while (continueDialog) {
+            when (options()) {
+                1 -> question_1()
+                2 -> {
+                    question_2()
+                    continueDialog = false
+                }
+            }
+        }
+    }
+
+    suspend fun QueueTask.options(): Int = options(
+        "Yes please.",
+        "No thank you.",
+    )
+
+    suspend fun QueueTask.question_1() {
+        chatPlayer("Yes please.", animation = 568)
+        open_shop(player)
+    }
+
+    suspend fun QueueTask.question_2() {
+        chatPlayer("No thank you.", animation = 568)
+    }
+
+fun open_shop(p: Player) {
+    p.openShop("Rommiks Crafty Supplies")
+}
