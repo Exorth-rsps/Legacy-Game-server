da9786106ca9bdd16aebcfc0eb12c2eddba34837
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-21 11:52:06 +0200

    Create dragons_dungeon.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/dragons_dungeon/spawn/dragons_dungeon.plugin.kts
@@ -0,0 +1,21 @@
+package org.alter.plugins.content.area.legacy.dragons_dungeon.spawn
+
+spawn_npc(Npcs.GREEN_DRAGON, 3032, 10245, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.GREEN_DRAGON, 3028, 10250, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.GREEN_DRAGON, 3037, 10246, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.GREEN_DRAGON, 3030, 10254, 0, 5, Direction.SOUTH)
+
+spawn_npc(Npcs.BLUE_DRAGON, 3031, 10259, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.BLUE_DRAGON, 3037, 10260, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.BLUE_DRAGON, 3039, 10255, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.BLUE_DRAGON, 3044, 10251, 0, 5, Direction.SOUTH)
+
+spawn_npc(Npcs.RED_DRAGON, 3049, 10254, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.RED_DRAGON, 3050, 10259, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.RED_DRAGON, 3044, 10261, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.RED_DRAGON, 3048, 10264, 0, 5, Direction.SOUTH)
+
+spawn_npc(Npcs.BLACK_DRAGON, 3049, 10268, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.BLACK_DRAGON, 3054, 10267, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.BLACK_DRAGON, 3054, 10272, 0, 5, Direction.SOUTH)
+spawn_npc(Npcs.BLACK_DRAGON, 3050, 10273, 0, 5, Direction.SOUTH)
