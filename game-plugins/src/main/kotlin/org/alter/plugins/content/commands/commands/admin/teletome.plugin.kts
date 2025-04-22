554abd0b5261ec60bf0438c18c54a5586f1ccf95
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-22 22:38:15 +0200

    Create teletome.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/admin/teletome.plugin.kts
@@ -0,0 +1,21 @@
+package org.alter.plugins.content.commands.commands.admin
+
+import org.alter.game.model.priv.Privilege
+import org.alter.plugins.content.commands.Commands_plugin.Command.tryWithUsage
+
+on_command("teletome", Privilege.ADMIN_POWER, description = "Teleport a player to you") {
+    val args = player.getCommandArgs()
+    tryWithUsage(player, args, "Invalid format! Example of proper command <col=801700>::telehere PlayerName</col>") { values ->
+        val targetPlayerName = values[0]
+        val targetPlayer = world.getPlayerForName(targetPlayerName)
+
+        if (targetPlayer != null) {
+            // Teleport target player to your location
+            targetPlayer.moveTo(player.tile)
+        } else {
+            player.message("<col=801700>Player not found!</col>")
+        }
+    }
+}
+
+
