46f8690bee9b7c4c9f47d25999bd2e37bac5a638
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-22 22:38:05 +0200

    Create teleto.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/admin/teleto.plugin.kts
@@ -0,0 +1,20 @@
+package org.alter.plugins.content.commands.commands.admin
+
+import org.alter.game.model.priv.Privilege
+import org.alter.plugins.content.commands.Commands_plugin.Command.tryWithUsage
+
+on_command("teleto", Privilege.ADMIN_POWER, description = "Teleport to another player") {
+    val args = player.getCommandArgs()
+    tryWithUsage(player, args, "Invalid format! Example of proper command <col=801700>::teleto PlayerName</col>") { values ->
+        val targetPlayerName = values[0]
+        val targetPlayer = world.getPlayerForName(targetPlayerName)
+
+        if (targetPlayer != null) {
+            // Teleport to target player’s location
+            player.moveTo(targetPlayer.tile)
+        } else {
+            player.message("<col=801700>Player not found!</col>")
+        }
+    }
+}
+
