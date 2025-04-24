ac8151f0e11d633b75f3511735ec33b7cd34391d
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-24 21:19:42 +0200

    Update teleports_player.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/player/teleports_player.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/commands/commands/player/teleports_player.plugin.kts
@@ -10,27 +10,27 @@ on_command("home", description = "Teleports you home") {
     val home = world.gameContext.home
     player.moveTo(home)
 }
-on_command("edge", description = "Teleports you to Edgeville") {
-    player.moveTo(Tile(x = 3087, z = 3499, height = 0))
-}
-on_command("ardy", description = "Teleports you to East Ardougne") {
-    player.moveTo(Tile(x = 2662, z = 3307, height = 0))
-}
-on_command("wardy", description = "Teleports you to West Ardougne") {
-    player.moveTo(Tile(x = 2534, z = 3306, height = 0))
-}
-on_command("cath", description = "Teleports you to Catherby") {
-    player.moveTo(Tile(x = 2804, z = 3433, height = 0))
-}
-on_command("tav", description = "Teleports you to Taverley") {
-    player.moveTo(Tile(x = 2891, z = 3454, height = 0))
-}
-on_command("fish", description = "Teleports you to Fishing Guild") {
-    player.moveTo(Tile(x = 2597, z = 3409, height = 0))
-}
-on_command("legends", description = "Teleports you to Legends Guild") {
-    player.moveTo(Tile(x = 2728, z = 3347, height = 0))
-}
-on_command("dragons", description = "Teleports you to the Dragons Dungeon") {
-    player.moveTo(Tile(x = 3235, z = 9366, height = 0))
-}
+//on_command("edge", description = "Teleports you to Edgeville") {
+//    player.moveTo(Tile(x = 3087, z = 3499, height = 0))
+//}
+//on_command("ardy", description = "Teleports you to East Ardougne") {
+//    player.moveTo(Tile(x = 2662, z = 3307, height = 0))
+//}
+//on_command("wardy", description = "Teleports you to West Ardougne") {
+//    player.moveTo(Tile(x = 2534, z = 3306, height = 0))
+//}
+//on_command("cath", description = "Teleports you to Catherby") {
+//    player.moveTo(Tile(x = 2804, z = 3433, height = 0))
+//}
+//on_command("tav", description = "Teleports you to Taverley") {
+//    player.moveTo(Tile(x = 2891, z = 3454, height = 0))
+//}
+//on_command("fish", description = "Teleports you to Fishing Guild") {
+//    player.moveTo(Tile(x = 2597, z = 3409, height = 0))
+//}
+//on_command("legends", description = "Teleports you to Legends Guild") {
+//    player.moveTo(Tile(x = 2728, z = 3347, height = 0))
+//}
+//on_command("dragons", description = "Teleports you to the Dragons Dungeon") {
+//    player.moveTo(Tile(x = 3235, z = 9366, height = 0))
+//}
