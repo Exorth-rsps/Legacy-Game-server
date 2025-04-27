74e41c9d20ca6a213b408f51917cf2244a5f3f27
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-27 20:45:47 +0200

    Create farmer_lvl7.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/npcs/human/farmer_lvl7.plugin.kts
@@ -0,0 +1,67 @@
+package org.alter.plugins.content.npcs.human;
+
+import org.alter.plugins.content.drops.DropTableFactory
+
+val ids = intArrayOf(
+    Npcs.FARMER_3243
+)
+
+val table = DropTableFactory
+val droptable =
+    table.build {
+        guaranteed {
+            obj(Items.BONES, quantity = 1)
+        }
+    }
+
+
+table.register(droptable, *ids)
+
+on_npc_pre_death(*ids) {
+    val p = npc.damageMap.getMostDamage()!! as Player
+}
+
+on_npc_death(*ids) {
+    table.getDrop(world, npc.damageMap.getMostDamage()!! as Player, npc.id, npc.tile)
+}
+
+
+ids.forEach {
+    set_combat_def(it) {
+        configs {
+            attackSpeed = 6
+            respawnDelay = 20
+            poisonChance = 0.0
+            venomChance = 0.0
+        }
+        stats {
+            hitpoints = 12
+            attack = 3
+            strength = 4
+            defence = 8
+            magic = 1
+            ranged = 1
+        }
+
+        bonuses {
+            defenceStab = 0
+            defenceSlash = 0
+            defenceCrush = 0
+            defenceMagic = 0
+            defenceRanged = 0
+        }
+
+
+        anims {
+            attack = Animation.HUMAN_SLASH_SWORD_ATTACK
+            block = Animation.HUMAN_SHIELD_DEFEND
+            death = Animation.HUMAN_DEATH
+        }
+
+        sound {
+            attackSound = Sound.HUMAN_ATTACK
+            blockSound = Sound.HUMAN_BLOCK_1
+            deathSound = Sound.HUMAN_DEATH
+        }
+    }
+}
