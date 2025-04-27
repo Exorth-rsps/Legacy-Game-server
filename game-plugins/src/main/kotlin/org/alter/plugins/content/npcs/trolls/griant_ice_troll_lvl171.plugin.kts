bef9d982ca66add39aad3d4aba7ca133f06c18b2
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-27 16:08:52 +0200

    Create griant_ice_troll_lvl171.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/npcs/trolls/griant_ice_troll_lvl171.plugin.kts
@@ -0,0 +1,83 @@
+package org.alter.plugins.content.npcs.trolls;
+
+import org.alter.plugins.content.combat.isBeingAttacked
+import org.alter.plugins.content.drops.DropTableFactory
+
+val ids = intArrayOf(
+    Npcs.ICE_TROLL
+)
+
+val table = DropTableFactory
+val droptable =
+    table.build {
+        guaranteed {
+            obj(Items.BIG_BONES, quantity = 1)
+        }
+        main {
+            total(4096)
+            obj(Items.RAW_SHRIMPS_NOTED, quantity = 1, slots = 32)
+            obj(Items.RAW_TROUT_NOTED, quantity = 1, slots = 32)
+            obj(Items.RAW_TUNA_NOTED, quantity = 1, slots = 16)
+            obj(Items.RAW_LOBSTER_NOTED, quantity = 1, slots = 16)
+            obj(Items.RAW_SWORDFISH_NOTED, quantity = 1, slots = 8)
+            obj(Items.FROZEN_KEY, quantity = 1, slots = 1)
+            nothing(512)
+        }
+        }
+
+
+
+
+table.register(droptable, *ids)
+on_npc_pre_death(*ids) {
+    val p = npc.damageMap.getMostDamage()!! as Player
+    val playerName = p.username
+    p.world.players.forEach {
+        it.message("<col=8900331>[GLOBAL]</col> The Giant Ice Troll has been slain by $playerName!", ChatMessageType.CONSOLE)
+    }
+
+}
+
+on_npc_death(*ids) {
+    table.getDrop(world, npc.damageMap.getMostDamage()!! as Player, npc.id, npc.tile)
+}
+
+
+ids.forEach {
+    set_combat_def(it) {
+        configs {
+            attackSpeed = 4
+            respawnDelay = 300
+            poisonChance = 0.0
+            venomChance = 0.0
+        }
+        stats {
+            hitpoints = 280
+            attack = 180
+            strength = 190
+            defence = 180
+            magic = 75
+            ranged = 75
+        }
+
+        bonuses {
+            defenceStab = 30
+            defenceSlash = 60
+            defenceCrush = 30
+            defenceMagic = 75
+            defenceRanged = 75
+        }
+
+        anims {
+            attack = Animation.TROLL_ATTACK
+            block = Animation.TROLL_DEFEND
+            death = Animation.TROLL_DEATH
+        }
+
+        sound {
+            attackSound = Sound.TROLL_ATTACK
+            blockSound = Sound.TROLL_HIT
+            deathSound = Sound.TROLL_DEATH
+        }
+    }
+}
