d18f83b750431459312947b8fb5c4d2c0ad1e408
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-04-28 18:49:02 +0200

    hotfix

--- game-plugins/src/main/kotlin/org/alter/plugins/service/restapi/RestApiService.kt
+++ game-plugins/src/main/kotlin/org/alter/plugins/service/restapi/RestApiService.kt
@@ -6,20 +6,35 @@ import org.alter.game.service.Service
 import org.alter.plugins.service.restapi.routes.CorsRoute
 import org.alter.plugins.service.restapi.routes.RestApiRoutes
 import gg.rsmod.util.ServerProperties
-import spark.Spark.stop
+import spark.Spark
 
 class RestApiService : Service {
     override fun init(server: Server, world: World, serviceProperties: ServerProperties) {
-        CorsRoute(serviceProperties.getOrDefault("origin", "*"), serviceProperties.getOrDefault("methods", "GET, POST"), serviceProperties.getOrDefault("headers", "X-PINGOTHER, Content-Type"))
+        // 1) HTTP-poort instellen (default 4567, of uit je properties)
+        val httpPort = serviceProperties.getOrDefault("port", "4567").toInt()
+        Spark.port(httpPort)
+
+        // 2) Serve een lege robots.txt om die 404-warning weg te halen
+        Spark.get("/robots.txt") { req, res ->
+            res.type("text/plain")
+            "User-agent: *\nDisallow:"
+        }
+
+        // 3) Je bestaande CORS-configuratie
+        CorsRoute(
+            serviceProperties.getOrDefault("origin", "*"),
+            serviceProperties.getOrDefault("methods", "GET, POST"),
+            serviceProperties.getOrDefault("headers", "X-PINGOTHER, Content-Type")
+        )
+
+        // 4) Je eigen API-routes
         RestApiRoutes().init(world, serviceProperties.getOrDefault("auth", false))
     }
 
     override fun postLoad(server: Server, world: World) {}
-
     override fun bindNet(server: Server, world: World) {}
 
     override fun terminate(server: Server, world: World) {
-        stop()
+        Spark.stop()
     }
-
 }
