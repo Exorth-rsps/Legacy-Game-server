79fa50c666f876c9096f068102634aea25f0b03b
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-05-04 13:45:44 +0200

    Update FishingSpots.kt

--- game-plugins/src/main/kotlin/org/alter/plugins/content/skills/fishing/data/FishingSpots.kt
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/skills/fishing/data/FishingSpots.kt
@@ -1,5 +1,6 @@
 package org.alter.plugins.content.skills.fishing.data
 
+import org.alter.api.cfg.Animation
 import org.alter.api.cfg.Items
 import org.alter.api.cfg.Npcs
 import org.alter.api.cfg.Objs
@@ -16,19 +17,20 @@ val FLYFISHING_ROD = arrayOf(Npcs.ROD_FISHING_SPOT_1527, Npcs.ROD_FISHING_SPOT_1
 //fishing spot 1523
 
 enum class FishingSpots(val spotEntityId: Int, val option: String, val toolId: Int, val baitId: Int, val animation: Int, vararg fish: Fish) {
-    TUTORIAL_NET(Npcs.FISHING_SPOT_3317, "net", Items.SMALL_FISHING_NET, -1, 621, Fish.SHRIMP),
-    NET(Npcs.FISHING_SPOT_1530, "net", Items.SMALL_FISHING_NET, -1, 621, Fish.SHRIMP, Fish.ANCHOVIES),
-    BAIT(Npcs.FISHING_SPOT_1530, "bait", Items.FISHING_ROD, Items.FISHING_BAIT, 622, Fish.SARDINE, Fish.HERRING),
-    BAIT2(Npcs.ROD_FISHING_SPOT_1527, "bait", Items.FISHING_ROD, Items.FISHING_BAIT, 622, Fish.PIKE),
-    BAIT3(Npcs.ROD_FISHING_SPOT_1526, "bait", Items.FISHING_ROD, Items.FISHING_BAIT, 622, Fish.PIKE),
-    LURE(Npcs.ROD_FISHING_SPOT_1527, "lure", Items.FLY_FISHING_ROD, Items.FEATHER, 622, Fish.TROUT, Fish.SALMON),
-    LURE2(Npcs.ROD_FISHING_SPOT_1526, "lure", Items.FLY_FISHING_ROD, Items.FEATHER, 622, Fish.TROUT, Fish.SALMON, Fish.RAINBOWFISH),
-    BIG_NET(Npcs.FISHING_SPOT_1511, "net", Items.BIG_FISHING_NET, -1, 621, Fish.MACKEREL, Fish.COD, Fish.BASS),
-    BIG_NET2(Npcs.FISHING_SPOT_1520, "big net", Items.BIG_FISHING_NET, -1, 621, Fish.MACKEREL, Fish.COD, Fish.BASS),
-    CAGE(Npcs.FISHING_SPOT_1522, "cage", Items.LOBSTER_POT, -1, 621, Fish.LOBSTER),
-    HARPOON(Npcs.FISHING_SPOT_1522, "harpoon", Items.HARPOON, -1, 621, Fish.TUNA, Fish.SWORDFISH),
-    HARPOON2(Npcs.FISHING_SPOT_5821, "harpoon", Items.HARPOON, -1, 621, Fish.SHARK),
-    NET2(Npcs.FISHING_SPOT_5821, "net", Items.SMALL_FISHING_NET, -1, 621, Fish.MONKFISH);
+    TUTORIAL_NET(Npcs.FISHING_SPOT_3317, "net", Items.SMALL_FISHING_NET, -1, Animation.FISHING_NET, Fish.SHRIMP),
+    NET(Npcs.FISHING_SPOT_1530, "net", Items.SMALL_FISHING_NET, -1, Animation.FISHING_NET, Fish.SHRIMP, Fish.ANCHOVIES),
+    BAIT(Npcs.FISHING_SPOT_1530, "bait", Items.FISHING_ROD, Items.FISHING_BAIT, Animation.FISHING_ROD, Fish.SARDINE, Fish.HERRING),
+    BAIT2(Npcs.ROD_FISHING_SPOT_1527, "bait", Items.FISHING_ROD, Items.FISHING_BAIT, Animation.FISHING_ROD, Fish.PIKE),
+    BAIT3(Npcs.ROD_FISHING_SPOT_1526, "bait", Items.FISHING_ROD, Items.FISHING_BAIT, Animation.FISHING_ROD, Fish.PIKE),
+    LURE(Npcs.ROD_FISHING_SPOT_1527, "lure", Items.FLY_FISHING_ROD, Items.FEATHER, Animation.FISHING_ROD, Fish.TROUT, Fish.SALMON),
+    LURE2(Npcs.ROD_FISHING_SPOT_1526, "lure", Items.FLY_FISHING_ROD, Items.FEATHER, Animation.FISHING_ROD, Fish.TROUT, Fish.SALMON, Fish.RAINBOWFISH),
+    BIG_NET(Npcs.FISHING_SPOT_1511, "net", Items.BIG_FISHING_NET, -1, Animation.FISHING_NET, Fish.MACKEREL, Fish.COD, Fish.BASS),
+    BIG_NET2(Npcs.FISHING_SPOT_1520, "big net", Items.BIG_FISHING_NET, -1, Animation.FISHING_NET, Fish.MACKEREL, Fish.COD, Fish.BASS),
+    BIG_NET3(Npcs.FISHING_SPOT_1511, "big net", Items.BIG_FISHING_NET, -1, Animation.FISHING_NET, Fish.MACKEREL, Fish.COD, Fish.BASS),
+    CAGE(Npcs.FISHING_SPOT_1522, "cage", Items.LOBSTER_POT, -1, Animation.FISHING_LOBSTER_POT, Fish.LOBSTER),
+    HARPOON(Npcs.FISHING_SPOT_1522, "harpoon", Items.HARPOON, -1, Animation.FISHING_HARPOON, Fish.TUNA, Fish.SWORDFISH),
+    HARPOON2(Npcs.FISHING_SPOT_5821, "harpoon", Items.HARPOON, -1, Animation.FISHING_HARPOON, Fish.SHARK),
+    NET2(Npcs.FISHING_SPOT_5821, "net", Items.SMALL_FISHING_NET, -1, Animation.FISHING_NET, Fish.MONKFISH);
     private val fish = fish
 
     fun getFish(): Array<out Fish> { return this.fish
