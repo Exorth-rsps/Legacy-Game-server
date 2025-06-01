7907f8d9201102ea778b43f511b36764e42a23de
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-01 20:38:42 +0200

    Create spawns.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/entrana/spawn/spawns.plugin.kts
@@ -0,0 +1,70 @@
+package org.alter.plugins.content.area.legacy.entrana.spawn
+
+spawn_npc(Npcs.FRINCOS, 2808, 3344, 0, 3, Direction.EAST)
+spawn_npc(Npcs.ODOVACAR, 2845, 3335, 0, 3, Direction.WEST)
+spawn_npc(Npcs.UNGADULU, 2851, 3371, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DRUID, 2818, 3344, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DRUID, 2812, 3344, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DRUID, 2827, 3344, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DRUID, 2831, 3344, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DRUID, 2836, 3344, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DRUID, 2827, 3340, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DRUID, 2827, 3347, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DRUID, 2827, 3352, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.MONK_2579, 2842, 3348, 0, 3, Direction.EAST)
+spawn_npc(Npcs.MONK_2579, 2844, 3349, 0, 3, Direction.EAST)
+spawn_npc(Npcs.MONK_2579, 2847, 3348, 0, 3, Direction.EAST)
+spawn_npc(Npcs.MONK_2579, 2849, 3350, 0, 3, Direction.EAST)
+spawn_npc(Npcs.MONK_2579, 2851, 3352, 0, 3, Direction.EAST)
+spawn_npc(Npcs.MONK_2579, 2855, 3350, 0, 3, Direction.EAST)
+spawn_npc(Npcs.MONK_2579, 2855, 3348, 0, 3, Direction.EAST)
+spawn_npc(Npcs.MONK_2579, 2851, 3346, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.CHAOS_DRUID, 2839, 3335, 0, 3, Direction.EAST)
+spawn_npc(Npcs.CHAOS_DRUID, 2836, 3335, 0, 3, Direction.EAST)
+spawn_npc(Npcs.CHAOS_DRUID, 2834, 3335, 0, 3, Direction.EAST)
+spawn_npc(Npcs.CHAOS_DRUID, 2831, 3335, 0, 3, Direction.EAST)
+spawn_npc(Npcs.CHAOS_DRUID, 2828, 3335, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 2811, 3355, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 2811, 3355, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 2815, 3337, 0, 3, Direction.EAST)
+spawn_npc(Npcs.DEADLY_RED_SPIDER, 2819, 3335, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.CAVE_CRAWLER_406, 2825, 3359, 0, 3, Direction.EAST)
+spawn_npc(Npcs.CAVE_CRAWLER_406, 2827, 3357, 0, 3, Direction.EAST)
+spawn_npc(Npcs.CAVE_CRAWLER_406, 2830, 3356, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.CAVE_CRAWLER_406, 2849, 3337, 0, 3, Direction.EAST)
+spawn_npc(Npcs.CAVE_CRAWLER_406, 2853, 3337, 0, 3, Direction.EAST)
+spawn_npc(Npcs.CAVE_CRAWLER_406, 2856, 3331, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.MONK_OF_ZAMORAK_529, 2857, 3336, 0, 3, Direction.EAST)
+spawn_npc(Npcs.MONK_OF_ZAMORAK_529, 2859, 3337, 0, 3, Direction.EAST)
+spawn_npc(Npcs.MONK_OF_ZAMORAK_529, 2861, 3336, 0, 3, Direction.EAST)
+spawn_npc(Npcs.MONK_OF_ZAMORAK_529, 2863, 3337, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.OGRE_2233, 2857, 3369, 0, 3, Direction.EAST)
+spawn_npc(Npcs.OGRE_2233, 2862, 3367, 0, 3, Direction.EAST)
+spawn_npc(Npcs.OGRE_2233, 2866, 3369, 0, 3, Direction.EAST)
+spawn_npc(Npcs.OGRE_2233, 2863, 3373, 0, 3, Direction.EAST)
+spawn_npc(Npcs.OGRE_2233, 2858, 3374, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.ZOMBIE_2501, 2845, 3382, 0, 3, Direction.EAST)
+spawn_npc(Npcs.ZOMBIE_2501, 2847, 3381, 0, 3, Direction.EAST)
+spawn_npc(Npcs.ZOMBIE_2501, 2848, 3384, 0, 3, Direction.EAST)
+spawn_npc(Npcs.ZOMBIE_2501, 2846, 3386, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.GOAT, 2836, 3369, 0, 3, Direction.EAST)
+spawn_npc(Npcs.GOAT, 2832, 3368, 0, 3, Direction.EAST)
+spawn_npc(Npcs.GOAT, 2832, 3371, 0, 3, Direction.EAST)
+spawn_npc(Npcs.GOAT, 2836, 3372, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.GORAK, 2813, 3381, 0, 3, Direction.EAST)
+spawn_npc(Npcs.GORAK, 2809, 3378, 0, 3, Direction.EAST)
+spawn_npc(Npcs.GORAK, 2826, 3378, 0, 3, Direction.EAST)
+spawn_npc(Npcs.GORAK, 2828, 3371, 0, 3, Direction.EAST)
+
+spawn_npc(Npcs.TWIGGY_OKORN, 2819, 3353, 0, 3, Direction.EAST)
+spawn_npc(Npcs.ZAFF, 2850, 3355, 0, 2, Direction.SOUTH)
