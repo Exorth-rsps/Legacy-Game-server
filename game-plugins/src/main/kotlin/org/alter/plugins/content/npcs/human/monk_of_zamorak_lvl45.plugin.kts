481d5b7dbf246e8ac5c57ee7a28de260d421b329
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-02 22:18:11 +0200

    Create monk_of_zamorak_lvl45.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/npcs/human/monk_of_zamorak_lvl45.plugin.kts
@@ -0,0 +1,75 @@
+package org.alter.plugins.content.npcs.human;
+
+import org.alter.plugins.content.drops.DropTableFactory
+
+val ids = intArrayOf(
+    Npcs.MONK_OF_ZAMORAK_529
+)
+
+val table = DropTableFactory
+val droptable =
+    table.build {
+        guaranteed {
+            obj(Items.BONES, quantity = 1)
+        }
+        main {
+            total(128)
+            nothing(16)
+        }
+        table("Herbs") {
+            total(512)
+            nothing(slots = 4)
+        }
+        table("Secondaries") {
+            total(256)
+            nothing(64)
+        }
+        table("Coins") {
+            total(256)
+            nothing(64)
+        }
+    }
+
+
+table.register(droptable, *ids)
+
+on_npc_pre_death(*ids) {
+    val p = npc.damageMap.getMostDamage()!! as Player
+}
+
+on_npc_death(*ids) {
+    table.getDrop(world, npc.damageMap.getMostDamage()!! as Player, npc.id, npc.tile)
+}
+
+
+ids.forEach {
+    set_combat_def(it) {
+        configs {
+            attackSpeed = 4
+            respawnDelay = 30
+            poisonChance = 0.0
+            venomChance = 0.0
+        }
+        stats {
+            hitpoints = 40
+            attack = 38
+            strength = 38
+            defence = 42
+            magic = 40
+            ranged = 1
+        }
+
+
+        anims {
+            attack = Animation.HUMAN_PUNCH
+            block = Animation.HUMAN_DEFEND
+            death = Animation.HUMAN_DEATH
+        }
+
+        sound {
+            attackSound = Sound.HUMAN_ATTACK
+            blockSound = Sound.HUMAN_BLOCK_1
+            deathSound = Sound.HUMAN_DEATH
+        }
+    }
+}
