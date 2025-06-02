45b3984b028760173d32fdc23746820467721ba2
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-02 22:48:16 +0200

    Create ungadulu_lvl70.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/npcs/human/ungadulu_lvl70.plugin.kts
@@ -0,0 +1,86 @@
+package org.alter.plugins.content.npcs.human;
+
+import org.alter.plugins.content.drops.DropTableFactory
+
+val ids = intArrayOf(
+    Npcs.UNGADULU
+)
+
+val table = DropTableFactory
+val droptable =
+    table.build {
+        guaranteed {
+            obj(Items.BONES, quantity = 1)
+        }
+        main {
+            total(128)
+            nothing(16)
+        }
+        table("Herbs") {
+            total(512)
+            nothing(slots = 4)
+        }
+        table("Herbs_Noted") {
+            total(256)
+            nothing(64)
+        }
+        table("Coins") {
+            total(256)
+            nothing(64)
+        }
+    }
+
+
+table.register(droptable, *ids)
+
+on_npc_pre_death(*ids) {
+    val p = npc.damageMap.getMostDamage()!! as Player
+    val playerName = p.username
+    p.world.players.forEach {
+        it.message("<col=8900331>[GLOBAL]</col> Ungadulu has been slain by $playerName!", ChatMessageType.CONSOLE)
+    }
+}
+
+on_npc_death(*ids) {
+    table.getDrop(world, npc.damageMap.getMostDamage()!! as Player, npc.id, npc.tile)
+}
+
+
+ids.forEach {
+    set_combat_def(it) {
+        configs {
+            attackSpeed = 4
+            respawnDelay = 25
+            poisonChance = 0.0
+            venomChance = 0.0
+        }
+        stats {
+            hitpoints = 230
+            attack = 57
+            strength = 61
+            defence = 65
+            magic = 65
+            ranged = 0
+        }
+        bonuses {
+            defenceStab = 0
+            defenceSlash = 0
+            defenceCrush = 0
+            defenceMagic = 55
+            defenceRanged = 0
+        }
+
+
+        anims {
+            attack = Animation.HUMAN_STAFF_BASH
+            block = Animation.HUMAN_STAFF_DEFEND
+            death = Animation.HUMAN_DEATH
+        }
+
+        sound {
+            attackSound = Sound.HUMAN_ATTACK
+            blockSound = Sound.HUMAN_HIT4
+            deathSound = Sound.HUMAN_DEATH
+        }
+    }
+}
