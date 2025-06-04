469a42ed0e9f4f1a9489e6911d0f34014baab369
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-04 20:28:29 +0200

    mortton update

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/mortton/spawn/spawns.plugin.kts
@@ -0,0 +1,6 @@
+package org.alter.plugins.content.area.legacy.mortton.spawn
+
+spawn_npc(Npcs.ODOVACAR, 3496, 3289, 0, 0, Direction.WEST)
+spawn_npc(Npcs.SHOP_KEEPER_2884, 3488, 3297, 0, 0, Direction.SOUTH)
+spawn_npc(Npcs.MONK_OF_ENTRANA_1166, 3522, 3285, 0, 0, Direction.WEST)
+
