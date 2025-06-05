bceb7e6d662fc179fb9f588772028553ed5c6f62
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-05 22:27:36 +0200

    Update Barrows.kt

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/barrows/Barrows.kt
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/barrows/Barrows.kt
@@ -11,12 +11,12 @@ object Barrows {
     data class Brother(val id: Int, val mound: Tile, val crypt: Tile)
 
     val BROTHERS = listOf(
-        Brother(org.alter.api.cfg.Npcs.DHAROK_THE_WRETCHED, Tile(3575, 3297), Tile(3575, 9703, 3)),
-        Brother(org.alter.api.cfg.Npcs.VERAC_THE_DEFILED, Tile(3556, 3297), Tile(3556, 9701, 3)),
-        Brother(org.alter.api.cfg.Npcs.AHRIM_THE_BLIGHTED, Tile(3565, 3288), Tile(3565, 9708, 3)),
-        Brother(org.alter.api.cfg.Npcs.GUTHAN_THE_INFESTED, Tile(3575, 3282), Tile(3575, 9694, 3)),
-        Brother(org.alter.api.cfg.Npcs.KARIL_THE_TAINTED, Tile(3565, 3275), Tile(3565, 9683, 3)),
-        Brother(org.alter.api.cfg.Npcs.TORAG_THE_CORRUPTED, Tile(3553, 3282), Tile(3553, 9689, 3)),
+        Brother(org.alter.api.cfg.Npcs.DHAROK_THE_WRETCHED, Tile(3575, 3297), Tile(3556, 9718, 3)), //20720
+        Brother(org.alter.api.cfg.Npcs.VERAC_THE_DEFILED, Tile(3556, 3297), Tile(3578, 9706, 3)), //20772
+        Brother(org.alter.api.cfg.Npcs.AHRIM_THE_BLIGHTED, Tile(3565, 3288), Tile(3557, 9703, 3)), //20770
+        Brother(org.alter.api.cfg.Npcs.GUTHAN_THE_INFESTED, Tile(3575, 3282), Tile(3534, 9704, 3)), //20722
+        Brother(org.alter.api.cfg.Npcs.KARIL_THE_TAINTED, Tile(3565, 3275), Tile(3546, 9684, 3)), //20771
+        Brother(org.alter.api.cfg.Npcs.TORAG_THE_CORRUPTED, Tile(3553, 3282), Tile(3568, 9683, 3)), //20721
     )
 
     const val CHEST = 20723
