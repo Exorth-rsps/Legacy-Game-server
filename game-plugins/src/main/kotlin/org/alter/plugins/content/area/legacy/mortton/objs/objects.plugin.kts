78150250b5baff940c3a902f27373714da6c52d3
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-05 18:39:31 +0200

    Create objects.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/mortton/objs/objects.plugin.kts
@@ -0,0 +1,18 @@
+package org.alter.plugins.content.area.legacy.mortton.objs
+
+/**
+ * @author Eikenb00m <https://github.com/eikenb00m>
+ */
+on_obj_option(obj = Objs.STAIRS_4919, option = "Walk-Down") {
+    player.moveTo(3361, 10274, 0)
+}
+on_obj_option(obj = Objs.EXIT_47149, option = "Climb-Up") {
+    player.moveTo(3454, 3242, 0)
+}
+on_obj_option(obj = Objs.CAVE_47148, option = "Exit") {
+    player.moveTo(3454, 3242, 0)
+}
+on_obj_option(obj = Objs.CAVE_47147, option = "Exit") {
+    player.moveTo(3454, 3242, 0)
+}
+
