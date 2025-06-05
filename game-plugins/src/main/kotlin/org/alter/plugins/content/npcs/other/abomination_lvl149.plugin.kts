9be449f938a50a3facae6db4f05d80a0acc8148d
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-05 18:54:53 +0200

    Create abomination_lvl149.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/npcs/other/abomination_lvl149.plugin.kts
@@ -0,0 +1,80 @@
+package org.alter.plugins.content.npcs.other
+
+import org.alter.plugins.content.combat.isBeingAttacked
+import org.alter.plugins.content.drops.DropTableFactory
+
+set_multi_combat_region(region = 13472)
+
+spawn_npc(Npcs.ABOMINATION_8262, x = 2274, z = 4698, walkRadius = 5)
+
+val ids = intArrayOf(
+    Npcs.ABOMINATION_8262
+)
+val table = DropTableFactory
+val npc =
+    table.build {
+        guaranteed {
+        }
+        main {
+            total(10240)
+
+        }
+
+    }
+
+table.register(npc, *ids)
+
+on_npc_pre_death(*ids) {
+    val p = npc.damageMap.getMostDamage()!! as Player
+    val playerName = p.username
+    p.world.players.forEach {
+        it.message("<col=8900331>[GLOBAL]</col> The Abomination has been slain by $playerName!", ChatMessageType.CONSOLE)
+    }
+}
+on_npc_death(*ids) {
+    table.getDrop(world, npc.damageMap.getMostDamage()!! as Player, npc.id, npc.tile)
+}
+ids.forEach {
+    set_combat_def(it) {
+
+        configs {
+            attackSpeed = 5
+            respawnDelay = 300
+            poisonChance = 0.0
+            venomChance = 0.0
+            followRange = 100
+        }
+        stats {
+            hitpoints = 200
+            attack = 110
+            strength = 110
+            defence = 110
+            magic = 110
+            ranged = 110
+        }
+
+        bonuses {
+            defenceStab = 80
+            defenceSlash = 80
+            defenceCrush = 80
+            defenceMagic = 20
+            defenceRanged = 180
+        }
+
+        anims {
+            attack = Animation.CHROMATIC_DRAGON_HIT
+            block = Animation.CHROMATIC_DRAGON_HIT
+            death = Animation.CHROMATIC_DRAGON_DEATH
+        }
+
+        sound {
+            attackSound = Sound.DRAGON_ATTACK
+            blockSound = Sound.DRAGON_HIT
+            deathSound = Sound.DRAGON_DEATH
+        }
+        aggro {
+            radius = 10
+        }
+
+    }
+}
