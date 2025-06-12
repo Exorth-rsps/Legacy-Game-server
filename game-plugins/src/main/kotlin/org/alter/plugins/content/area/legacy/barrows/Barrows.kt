91f9673b4b3385cb28b11b24e7109608c0347e5c
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-12 20:49:12 +0200

    Add combat configs for Barrows brothers

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/barrows/Barrows.kt
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/barrows/Barrows.kt
@@ -2,10 +2,12 @@ package org.alter.plugins.content.area.legacy.barrows
 
 import org.alter.game.model.attr.AttributeKey
 import org.alter.game.model.Tile
+import org.alter.api.cfg.Objs
 
 object Barrows {
     // bit flags for each brother in order: Dharok, Verac, Ahrim, Guthan, Karil, Torag
     val PROGRESS_ATTR = AttributeKey<Int>(persistenceKey = "barrows_progress")
+    val LAST_BROTHER_ATTR = AttributeKey<Int>(persistenceKey = "barrows_last")
 
     data class Brother(val id: Int, val mound: Tile, val crypt: Tile)
 
@@ -18,5 +20,15 @@ object Barrows {
         Brother(org.alter.api.cfg.Npcs.TORAG_THE_CORRUPTED, Tile(3553, 3282), Tile(3553, 9689, 3)),
     )
 
-    const val CHEST = 20973
+    const val CHEST = 20723
+    const val TUNNEL_INDEX = 5
+
+    val SARCOPHAGUS_IDS = intArrayOf(
+        Objs.SARCOPHAGUS_20720,
+        Objs.SARCOPHAGUS_20772,
+        Objs.SARCOPHAGUS_20770,
+        Objs.SARCOPHAGUS_20722,
+        Objs.SARCOPHAGUS_20771,
+        Objs.SARCOPHAGUS_20721
+    )
 }
