cc3979f05f0f13c891e340089c07d8d241298ce7
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-09-02 13:13:11 +0200

    Move ai_players config to root

--- game-server/src/main/kotlin/org/alter/game/service/AiPlayerConfig.kt
+++ game-server/src/main/kotlin/org/alter/game/service/AiPlayerConfig.kt
@@ -13,7 +13,7 @@ class AiPlayerConfig : Service {
     var names: List<String> = emptyList()
 
     override fun init(server: Server, world: World, serviceProperties: ServerProperties) {
-        val file = File("../data/cfg/ai_players.yml")
+        val file = File("../ai_players.yml")
         val props = ServerProperties().loadYaml(file)
         minOnline = props.getOrDefault("min_online", 0)
         maxOnline = props.getOrDefault("max_online", 0)
