3c7f63694481d3d5bba574800ab445b67953950d
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-09-30 21:24:54 +0200

    Create stairs.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/gnome_stronghold/objs/stairs.plugin.kts
@@ -0,0 +1,34 @@
+package org.alter.plugins.content.area.legacy.gnome_stronghold.objs
+/**
+ * @author Eikenb00m <https://github.com/eikenb00m>
+ */
+
+//on_obj_option(obj = Objs.STAIRCASE_16675, option = "climb-Up") {
+//    when(player.tile.x) {
+//        1 -> {
+//            player.moveTo(x = 2680, z = 1637, 2)
+//        }
+//        else ->  player.moveTo(x = 2680, z = 1642, 1)
+//    }
+//}
+//on_obj_option(obj = Objs.STAIRCASE_15648, option = "climb-down") {
+//    when(player.tile.height) {
+//        1 -> {
+//            player.moveTo(x = 2681, z = 1638, 0)
+//        }
+//        else ->  player.moveTo(x = 2680, z = 1633, 1)
+//    }
+//}
+//
+//on_obj_option(obj = Objs.STAIRCASE_15653, option = "Climb-Up") {
+//    when(player.tile.regionId) {
+//        11032 -> { // Legendsguild
+//            player.moveTo(x = 2767, z = 1596, 1)
+//        }
+//
+//        else -> player.message("Nothing interesting happens.")
+//    }
+//}
+
+
+
