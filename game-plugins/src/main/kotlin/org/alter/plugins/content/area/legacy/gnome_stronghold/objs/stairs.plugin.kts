2512ef0b799a64bf718be3633467b9954a9063eb
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-09-30 21:44:09 +0200

    Update stairs.plugin.kts

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/gnome_stronghold/objs/stairs.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/legacy/gnome_stronghold/objs/stairs.plugin.kts
@@ -1,34 +1,56 @@
 package org.alter.plugins.content.area.legacy.gnome_stronghold.objs
+
 /**
  * @author Eikenb00m <https://github.com/eikenb00m>
  */
 
-//on_obj_option(obj = Objs.STAIRCASE_16675, option = "climb-Up") {
-//    when(player.tile.x) {
-//        1 -> {
-//            player.moveTo(x = 2680, z = 1637, 2)
-//        }
-//        else ->  player.moveTo(x = 2680, z = 1642, 1)
-//    }
-//}
-//on_obj_option(obj = Objs.STAIRCASE_15648, option = "climb-down") {
-//    when(player.tile.height) {
-//        1 -> {
-//            player.moveTo(x = 2681, z = 1638, 0)
-//        }
-//        else ->  player.moveTo(x = 2680, z = 1633, 1)
-//    }
-//}
-//
-//on_obj_option(obj = Objs.STAIRCASE_15653, option = "Climb-Up") {
-//    when(player.tile.regionId) {
-//        11032 -> { // Legendsguild
-//            player.moveTo(x = 2767, z = 1596, 1)
-//        }
-//
-//        else -> player.message("Nothing interesting happens.")
-//    }
-//}
+on_obj_option(obj = Objs.STAIRCASE_16675, option = "climb-Up") {
+    when(player.tile.z) {
+        3414 -> { //bank
+            player.queue {
+                player.animate(828)
+                player.lock()
+                wait(2)
+                player.moveTo(x = 2445, z = 3416, 1)
+                player.unlock()
+            }
+        }
+        3434 -> { //bank
+            player.queue {
+                player.animate(828)
+                player.lock()
+                wait(2)
+                player.moveTo(x = 2445, z = 3433, 1)
+                player.unlock()
+            }
+        }
+        else ->  player.message("Nothing intresting happens...")
+    }
+}
+on_obj_option(obj = Objs.STAIRCASE_16677, option = "climb-Down") {
+    when(player.tile.z) {
+        3416 -> { //bank
+            player.queue {
+                player.animate(828)
+                player.lock()
+                wait(2)
+                player.moveTo(x = 2446, z = 3415, 0)
+                player.unlock()
+            }
+        }
+        3433 -> { //bank
+            player.queue {
+                player.animate(828)
+                player.lock()
+                wait(2)
+                player.moveTo(x = 2444, z = 3434, 0)
+                player.unlock()
+            }
+        }
+        else ->  player.message("Nothing intresting happens...")
+    }
+}
+
 
 
 
